de1d092e081397e29927b31f527fc31106587e8e
Abhishek Goyal <goyalabhi1305@gmail.com>
2021-08-06 23:29:46 +0530

    gen-work

---
+++ routes/data.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const router = express.Router()
+const StudentsProfile = require('../models/data')
+
+// [{
+// name: "Harry Potter and the Order of the Phoenix",
+// img: "https://bit.ly/2IcnSwz",
+// summary: "Harry Potter and Dumbledore's warning about the return of
+// Lord Voldemort is not heeded by the wizard authorities who, in turn, look to
+// undermine Dumbledore's authority at Hogwarts and discredit Harry."
+// }, {
+// name: "The Lord of the Rings: The Fellowship of the Ring",
+// img: "https://bit.ly/2tC1Lcg",
+// summary: "A young hobbit, Frodo, who has found the One Ring that
+// belongs to the Dark Lord Sauron, begins his journey with eight companions to
+// Mount Doom, the only place where it can be destroyed."
+// }, {
+// name: "Avengers: Endgame",
+// img: "https://bit.ly/2Pzczlb",
+// summary: "Adrift in space with no food or water, Tony Stark sends a
+// message to Pepper Potts as his oxygen supply starts to dwindle. Meanwhile, the
+// remaining Avengers -- Thor, Black Widow, Captain America, and Bruce Banner --
+// must figure out a way to bring back their vanquished allies for an epic showdown
+// with Thanos -- the evil demigod who decimated the planet and the universe."
+// }]
+router.get('/create', async (req, res) => {
+    try {
+        // console.log(topusers)
+        const insert = await new StudentsProfile({
+            name: 'Harry Potter and the Order of the Phoenix',
+            img: 'https://bit.ly/2IcnSwz',
+            string: `Harry Potter and Dumbledore's warning about the return of
+            Lord Voldemort is not heeded by the wizard authorities who, in turn, look to
+            undermine Dumbledore's authority at Hogwarts and discredit Harry.`
+        }).save()
+        if (insert) {
+        res.status(200).json({msg:'success' })
+        }
+        else {
+        res.status(500).json({msg:'An error occurred'})
+            
+}
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500).json({msg:'An error occurred'})
+        
+    }
+})
+
+router.get('/read', async (req, res) => {
+    try {
+        const data = await StudentsProfile.find()
+        if (data) {
+            res.status(200).json({msg:'success',data:data })
+            }
+            else {
+            res.status(500).json({msg:'An error occurred'})  
+    }
+    }
+    catch (e) {
+        
+        console.log(e)
+        res.status(500).json({msg:'An error occurred'})
+    }
+})
+
+
+router.get('/update', async (req, res) => {
+    try {
+        const data = await StudentsProfile.updateOne({name:'Harry Potter and the Order of the Phoenix'},{$set:{name:'Abhi'}})
+        if (data) {
+            res.status(200).json({msg:'success' })
+            }
+            else {
+            res.status(500).json({msg:'An error occurred'})  
+    }
+    }
+    catch (e) {
+        
+        console.log(e)
+        res.status(500).json({msg:'An error occurred'})
+    }
+})
+
+router.get('/delete', async (req, res) => {
+    try {
+        const data = await StudentsProfile.deleteOne({name:'Harry Potter and the Order of the Phoenix'})
+        if (data) {
+            res.status(200).json({msg:'success' })
+            }
+            else {
+            res.status(500).json({msg:'An error occurred'})  
+    }
+    }
+    catch (e) {
+        
+        console.log(e)
+        res.status(500).json({msg:'An error occurred'})
+    }
+})
+
+
+
+
+module.exports = router
